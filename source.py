eb3028cbe2f627b757dc002b097a1127270c7a23
Aleksejs Jurenoks <40733171+ajurenoks@users.noreply.github.com>
2024-01-27 12:00:33 +0200

    Create source.py

---
+++ source.py
@@ -0,0 +1,11 @@
+## WRITE THE PRINT_ITEMS FUNCTION HERE ##
+#                                       #
+#                                       #
+#                                       #
+#                                       #
+#########################################
+
+      
+        
+# DO NOT CHANGE THIS LINE:
+print_items(10)
